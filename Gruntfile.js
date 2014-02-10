7d782f759e37faac99f5762249823fd60e2c5d94
kosmobot <jeppe@burchardt.com>
2014-02-10 23:54:44 +0100

    add uglify to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,21 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		qunit: {
-			files: ['src/index.html']
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: [
+					'src/scripts/lib/jquery-1.11.0.js',
+					'src/scripts/lib/knockout-3.0.0.js',
+					'src/scripts/brewthumb.js',
+				],
+				dest: 'bin/scripts/<%= pkg.name %>.js'
+			}
 		},
 		jshint: {
-			files: ['Gruntfile.js', 'src/scripts/brewthumb.js'],
+			files: ['Gruntfile.js', 'src/scripts/*.js'],
 			options: {
 				globals: {
 					jQuery: true,
@@ -15,12 +25,33 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'bin/scripts/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint', 'concat', 'uglify', 'rename']
+		},
+		rename: {
+			moveHtml: {
+				src: 'src/index.html',
+				dest: 'bin/index.html'
+			},
+		}
 	});
 
+	grunt.loadNpmTasks('grunt-rename');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('test', ['jshint', 'qunit']);
-
-	grunt.registerTask('default', ['jshint', 'qunit']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'rename']);
 };
