e20be3bd67713c54c943cf0cf4e824c9c6c432df
Jeppe Burchardt <jeppe@burchardt.com>
2014-02-26 12:33:52 +0100

    add app icon resize grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,8 +48,27 @@ module.exports = function(grunt) {
 		copy: {
 			dist: {
 				files: [
-					{expand: true, flatten: true, src: ['src/index.html', 'src/resources/*.ico', 'src/resources/*.png', 'tmp/**.js', 'tmp/**.css'], 'dest': 'bin/', 'filter':'isFile'}
+					{expand: true, flatten: true, src: [
+						'src/index.html',
+						'src/resources/*.ico',
+						'src/resources/*.png',
+						'src/resources/*.xml',
+						'tmp/**.js',
+						'tmp/**.css'
+					], 'dest': 'bin/', 'filter':'isFile'}
 				]
+			}
+		},
+		multiresize: {
+			ios: {
+				src: 'src/resources/appicon.png',
+				dest: ['src/resources/appicon60.png', 'src/resources/appicon76.png', 'src/resources/appicon114.png', 'src/resources/appicon152.png'],
+				destSizes: ['60x60', '76x76', '114x114', '152x152']
+			},
+			windowsPhone: {
+				src: 'src/resources/appicon.png',
+				dest: ['src/resources/appicon70.png', 'src/resources/appicon150.png', 'src/resources/appicon310.png'],
+				destSizes: ['70x70', '150x150', '310x310']
 			}
 		}
 	});
@@ -59,6 +78,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-multiresize');
 
+	grunt.registerTask('icons', ['multiresize']);
 	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
 };
