80efd0ec812d5b38d8d844575a65892fb1694010
kosmobot <jeppe@burchardt.com>
2014-03-26 20:07:22 +0100

    implement save to local storage - work in progress

--- src/scripts/brewthumb.js
+++ src/scripts/brewthumb.js
@@ -312,6 +312,38 @@ var ViewModel = function() {
 
 };
 
+var viewModel = new ViewModel();
+
+var storedViewModel = localStorage.getItem("viewModel");
+if (storedViewModel) {
+	data = JSON.parse(storedViewModel);
+	console.log(settings.saveLocal());
+	
+	settings.saveLocal(data.settings.saveLocal);
+	if (settings.saveLocal()) {
+		viewModel.selectedTab(data.selectedTab);
+
+		viewModel.mashingView.cst_grainAmount(data.mashingView.cst_grainAmount);
+		viewModel.mashingView.cst_grainTemp(data.mashingView.cst_grainTemp);
+		viewModel.mashingView.cst_waterAmount(data.mashingView.cst_waterAmount);
+		viewModel.mashingView.cst_target(data.mashingView.cst_target);
+
+		//TODO: Add the rest of the views...	
+	}
+} 
 
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings(viewModel);
+
+
+function saveViewModel() {
+	if (settings.saveLocal()) {
+		localStorage.viewModel = ko.toJSON(viewModel);
+	}
+}
+window.addEventListener('beforeunload', saveViewModel);
+window.addEventListener('unload', saveViewModel);
+
+
+
+
